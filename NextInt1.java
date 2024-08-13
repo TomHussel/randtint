b07d06dae0f0950655907c74f3f7aabcee73f37e
TomHussel <marco.simas@gmail.com>
2024-08-13 22:31:13 +0100

    Add files via upload

---
+++ NextInt1.java
@@ -0,0 +1,26 @@
+/* public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+ */
+
+ // Java code to demonstrate the working 
+// of nextInt() 
+import java.util.*; 
+public class NextInt1
+{ 
+	public static void main(String[] args) 
+	{ 
+
+		// create random object 
+		Random ran = new Random(); 
+
+		// generating integer 
+		int nxt = ran.nextInt(); 
+
+		// Printing the random Number 
+		System.out.println 
+		("The Randomly generated integer is : " + nxt); 
+	} 
+} 
